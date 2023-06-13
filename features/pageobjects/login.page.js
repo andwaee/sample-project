91f833e9382f4f8bb1bbfd6bbe0f928a758d13ec
Adrian <johnadrian171@gmail.com>
2023-06-13 10:59:40 +0800

    updates

--- features/pageobjects/login.page.js
+++ features/pageobjects/login.page.js
@@ -3,13 +3,9 @@
 import Page from './page.js';
 import CommonUtility from '../common-utility/common-util.js';
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
+
 class LoginPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
+
     get inputUsername () {
         return $('#user-name');
     }
@@ -22,10 +18,7 @@ class LoginPage extends Page {
         return $('#login-button');
     }
 
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
+
     async login (username, password) {
         await this.inputUsername.setValue(username);
         await this.inputPassword.setValue(password);
@@ -33,9 +26,7 @@ class LoginPage extends Page {
         await this.btnSubmit.click();
     }
 
-    /**
-     * overwrite specific options to adapt it to page object
-     */
+
     open () {
         return super.open('https://www.saucedemo.com/');
     }
