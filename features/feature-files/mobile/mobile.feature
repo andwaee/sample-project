4a6ddd451b003ff2f0d63fef38bf9a2c77a881cc
Adrian <johnadrian171@gmail.com>
2023-08-13 00:34:40 +0800

    adding scenario

--- features/feature-files/mobile/mobile.feature
+++ features/feature-files/mobile/mobile.feature
@@ -1,9 +1,17 @@
 Feature: Mobile Functionality
 
     @Mobile
-    Scenario: As a user, navigate to Alert Dialog
+    Scenario: As a user, I can navigate to Alert Dialog
 
         Given App is already installed
         When I navigate to Alert Dialog
         Then I should see the Alert Dialog
+
+    @Mobile
+    Scenario: As a user, I can custom title
+
+        Given App is already installed
+        When I navigate to Custom Title screen
+        And I changed title fields
+        Then I should see the Custom Title is changed
 
--- features/pageobjects/mobile/mobile.screen.js
+++ features/pageobjects/mobile/mobile.screen.js
@@ -22,11 +22,65 @@ class MobileScreen {
         return $('/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/android.widget.TextView')
     }
 
+    get customTitleMenu(){
+        return $('~Custom Title');
+    }
+
+    get activityMenu(){
+        return $('~Activity');
+    }
+
+    get fields(){
+        return $$('android.widget.EditText');
+    }
+
+    get changeLeftButton(){
+        return $('~Change Left');
+    }
+
+    get changeRightButton(){
+        return $('~Change Right');
+    }
+
+    get leftText(){
+        return $$('android.widget.EditText')[0];
+    }
+
+    get rightText(){
+        return $$('android.widget.EditText')[1];
+    }
+
 
     async navigateToDialog(){
         await this.appMenu.click();
         await this.alertDialogMenu.click();
         await this.listDialogMenu.click();
+    }
+
+    async navigateToCustomTitleScreen(){
+        await this.appMenu.click();
+        await this.activityMenu.click();
+        await this.customTitleMenu.click();
+    }
+
+    async changeTitles(){
+        const leftFieldValue = 'Left Value is Changed';
+        const rightFieldValue = 'Right Value is Changed';
+        await this.fields[0].click();
+        await this.fields[0].clearValue();
+        await this.fields[0].addValue(leftFieldValue);
+        await this.changeLeftButton.click();
+        await this.fields[1].click();
+        await this.fields[1].clearValue();
+        await this.fields[1].addValue(rightFieldValue);
+        await this.changeRightButton.click();
+
+        return {leftFieldValue, rightFieldValue}
+    }
+
+    async validateFieldifChanged(leftText, rightText){
+        await expect(this.leftText).toHaveText(leftText);
+        await expect(this.rightText).toHaveText(rightText);
     }
 
 }
